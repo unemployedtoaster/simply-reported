2a5b9577c433297ebfd72b1a942d074182c874cb
bafso <bafso@gmail.comgit config --global user.name bafsogit config --global user.email bafso@gmail.comgit commit -m aternos>
2026-09-10 19:00:59 +0600

    fix login page jsx error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -23,7 +23,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
     async session({ session, token }) {
       if (session.user) {
-        session.user.id = token.sub!
+        (session.user as any).id = token.sub!
       }
       return session
     },
